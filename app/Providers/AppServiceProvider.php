08f7874553114e057633a4b6c1dacc80b1ede97f
Carlos Eduardo Vieira <carlosportalsocial@gmail.com>
2023-06-25 13:16:24 -0300

    added AppServiceProvider for task

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,8 @@
 
 namespace App\Providers;
 
+use App\Models\Task;
+use App\Repositories\TaskRepository;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -11,7 +13,11 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register(): void
     {
-        //
+        $this->app->bind('TaskRepositoryInterface', 'TaskRepository');
+
+        $this->app->bind('TaskRepositoryInterface', function(){
+            return new TaskRepository(new Task());
+        });
     }
 
     /**
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\TaskController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -14,5 +15,8 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    //return view('welcome');
+    return csrf_token(); // http://localhost:8000/ - pegar o token no postman
 });
+
+Route::get('tasks', [TaskController::class, 'getList']);
